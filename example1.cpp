cd95f0f8e183e4bf4a7ce36e9766f499faab4c43
凃浩然 <tu3394417523@gmail.com>
2021-07-05 19:16:22 +0800

    v2

--- example1.cpp
+++ example1.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
+#include<vector>
+#include<string>
+using std::string;
+using std::vector;
 using namespace std;
 int main(int argc, char**argv)
 {
     cout<<"hello world!"<<endl;
+    vector<int> Array{1,2,3,4};
+    for(auto i:Array)
+    {
+        cout<<i<<" ";
+    }
+    cout<<endl;
     return 0;
 }
