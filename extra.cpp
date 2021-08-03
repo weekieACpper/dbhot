31d225fb6768092399fad4595702cdaa2475bc85
凃浩然 <tu3394417523@gmail.com>
2021-08-03 17:12:15 +0800

    edit on linux

---
+++ extra.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-03 17:02:09
+ * @LastEditTime: 2021-08-03 17:03:23
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /dbhot/extra.cpp
+ */
+#include<iostream>
+int main(int argc, char**argv)
+{
+    std::cout<<"new function on dev branch"<<std::endl;
+    return 0;
+}
